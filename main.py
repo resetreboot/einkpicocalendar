eaba4dc7d7b2c445227090544e30a4bcc0e72b4c
José Carlos Cuevas <reset.reboot@gmail.com>
2021-11-24 15:51:19 +0100

    Changed font, now we can mark holidays on the calendar

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import utime
 import framebuf
 import calendar
 import font10
-import texgyread20
+import texgyread30 as texgyread
 
 from machine import Pin, ADC, SPI
 from epaper import EPD
@@ -13,6 +13,9 @@ CONVERSION_FACTOR = 3.3 / (65535)
 
 CALX = 80
 CALY = 60
+
+CALENDAR_WIDTH = 244
+CALENDAR_HEIGHT = 140
 
 WEEKDAYS = [
     "Lun",
@@ -24,24 +27,26 @@ WEEKDAYS = [
     "Dom",
 ]
 
+MARK_WEEKENDS = False
+
 # Display resolution
 EPD_WIDTH       = 400
 EPD_HEIGHT      = 300
 
 
-def writetext(fb, font, posx, posy, text, invert=False, format=framebuf.MONO_HMSB):
+def writetext(fb, font, posx, posy, text, invert=False, format=framebuf.MONO_HMSB, key=1):
     x = posx
     fheight = font.height()
     for letter in text:
         glyph, height, width = font.get_ch(letter)
         buf = bytearray(glyph)
 
-        if invert:
+        if not invert:
             for i, v in enumerate(buf):
                 buf[i] = 0xFF & ~ v
 
         frm = framebuf.FrameBuffer(buf, width, height, format)
-        fb.blit(frm, x, posy + (fheight - height), 1)
+        fb.blit(frm, x, posy + (fheight - height), key)
         x += width
 
 
@@ -54,6 +59,13 @@ def draw_calendar_outline(fb, posx, posy, width, height, color):
     lineincr = height // 7
     for line in range(1, 7):
         fb.hline(posx, posy + (line * lineincr), width, color)
+
+
+def draw_black_day(fb, posx, posy, width, height, day, week):
+    colpos = width // 7
+    linepos =  height // 7
+    extend = 5 if day == 6 else 0
+    fb.fill_rect(posx + (day * colpos), posy + (week * linepos), colpos + extend, linepos, 0)
 
 
 if __name__ == '__main__':
@@ -69,6 +81,21 @@ if __name__ == '__main__':
     buf = bytearray(EPD_HEIGHT * EPD_WIDTH)
     fb = framebuf.FrameBuffer(buf, EPD_WIDTH, EPD_HEIGHT, framebuf.MONO_HLSB)
 
+    holidays = {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: [],
+        10: [],
+        11: [],
+        12: [],
+    }
+
     print("Reading file with old date")
     try:
         olddata = open('date.txt')
@@ -82,6 +109,20 @@ if __name__ == '__main__':
         print("Not found")
         year = 2021
         month = 11
+
+    print("Reading holidays")
+    try:
+        holidata = open('holidays.txt')
+        print("Found")
+        for line in holidata:
+            dt = line.split(",")
+            month = int(dt[1])
+            holidays[month].append(int(dt[0]))
+
+        holidata.close()
+
+    except:
+        print("Not found")
 
     fb.fill(0xff)
     print("Starting temperature sensor")
@@ -128,25 +169,27 @@ if __name__ == '__main__':
             first_loop = False
             fb.fill(0xff)
             print("Writing temperature")
-            writetext(fb, texgyread20, 15, 10, "Temperatura: ", invert=True)
-            writetext(fb, texgyread20, 124, 10, "{:.1f}C".format(temperatura), invert=True)
+            writetext(fb, texgyread, 15, 10, "Temperatura: ")
+            writetext(fb, texgyread, 180, 10, "{:.1f}C".format(temperatura))
             print("Writing month")
-            writetext(fb, texgyread20, CALX, CALY, "{} - {}".format(month_name, year), invert=True)
+            writetext(fb, texgyread, CALX, CALY, "{} - {}".format(month_name, year))
             print("Writing header")
             count = 0
             for dayname in WEEKDAYS:
-                writetext(fb, font10, CALX + 4 + (count * 34), CALY + 40, dayname, invert=True, format=framebuf.MONO_HLSB)
+                writetext(fb, font10, CALX + 4 + (count * 34), CALY + 40, dayname, format=framebuf.MONO_HLSB)
                 count += 1
 
             print("Writing calendar outline")
-            draw_calendar_outline(fb, CALX, CALY + 35, 244, 140, 0)
+            draw_calendar_outline(fb, CALX, CALY + 35, CALENDAR_WIDTH, CALENDAR_HEIGHT, 0)
 
             print("Writing chevrons")
-            writetext(fb, texgyread20, 2, 185, "<", invert=True)
-            writetext(fb, texgyread20, 2, 265, ">", invert=True)
+            writetext(fb, texgyread, 2, 185, "<")
+            writetext(fb, texgyread, 2, 265, ">")
 
             days = 0
             week = 1
+            invert = True
+            usekey = 1
             print("Writing calendar days")
             for value in month_array:
                 if value:
@@ -157,7 +200,17 @@ if __name__ == '__main__':
                     else:
                         numtext = "{}".format(value)
 
-                    writetext(fb, font10, CALX + 10 + (days * 34), CALY + 40 + (week * 20), numtext, invert=True, format=framebuf.MONO_HLSB)
+                    if value in holidays[month] or (MARK_WEEKENDS and days > 4):
+                        # Draw loaded holidays in another format
+                        draw_black_day(fb, CALX, CALY + 35, CALENDAR_WIDTH, CALENDAR_HEIGHT, days, week)
+                        invert = True
+                        usekey = 0
+
+                    else:
+                        invert = False
+                        usekey = 1
+
+                    writetext(fb, font10, CALX + 10 + (days * 34), CALY + 38 + (week * 20), numtext, invert=invert, format=framebuf.MONO_HLSB, key=usekey)
 
                 days += 1
                 if days > 6:
